a65b4cab6100a09ce6d4a8a18e729563c36832de
iambigz <bigzgulf@gmail.com>
2019-05-02 13:51:38 +0700

    create init.sh
    create folder following structure

---
+++ init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p /docker/nagios/etc
+mkdir -p /docker/nagios/var
+mkdir -p /docker/nagios/customplugins
+mkdir -p /docker/nagios/nagiosgraphvar
+mkdir -p /docker/nagios/nagiosgraphetc
